f10943ffd41709eb140d3f3e0de10d7e26ade5b1
Cbas12 <105694795+Cbas12@users.noreply.github.com>
2024-01-18 22:57:14 -0500

    Update ingest_data.py

--- ingest_data.py
+++ ingest_data.py
@@ -13,9 +13,10 @@ def main(params):
     db = params.db
     table_name = params.table_name
     url = params.url
-    
-    csv_name = "output.csv"
 
+    #cargar data de taxis    
+    csv_name = "output.csv"
+    
     #download CSV
     os.system("wget "+str(url)+" -O "+str(csv_name))
 
@@ -44,17 +45,21 @@ def main(params):
         
         print("inserted another chunk..., tomó " + str(t_end-t_start))
 
+    #cargar archivo de zones
+    df_zones = pd.read_csv("taxi_zone_lookup.csv")
+    df_zones.to_sql(name="taxi_zone_lookup",con=engine, if_exists="replace")
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Ingest CSV data to Postgres")
 
-    parser.add_argument("--user",help="user name for postgres")
-    parser.add_argument("--password",help="password for postgres")
-    parser.add_argument("--host",help="host for postgres")
-    parser.add_argument("--port",help="port for postgres")
-    parser.add_argument("--db",help="database name for postgres")
-    parser.add_argument("--table_name",help="name of the table where we will write the results to")
-    parser.add_argument("--url",help="url of the csv file")
+    parser.add_argument("user",help="user name for postgres")
+    parser.add_argument("password",help="password for postgres")
+    parser.add_argument("host",help="host for postgres")
+    parser.add_argument("post",help="port for postgres")
+    parser.add_argument("db",help="database name for postgres")
+    parser.add_argument("table-name",help="name of the table where we will write the results to")
+    parser.add_argument("url",help="url of the csv file")
 
     args = parser.parse_args()
     main(args)
